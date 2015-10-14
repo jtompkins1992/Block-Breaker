8b964febef1923e7f9f3cfe1014fd681b4fd90c9
jtompkins1992 <jackhmtompkins@gmail.com>
2015-10-14 14:19:55 +0100

    Adding Initial Project

---
+++ blockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	private Paddle paddle;
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+	
+	// Use this for initialization
+	void Start () {
+		paddle = GameObject.FindObjectOfType<Paddle>();
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!hasStarted){
+			//Lock the ball relative to the paddle.
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+		
+			//Wait for mouse press relative to launch
+			if (Input.GetMouseButtonDown(0)){
+				hasStarted = true;
+				print ("Mouse Clicked, launch ball");
+				this.rigidbody2D.velocity = new Vector2(2f,10f);
+			}
+		}
+	}
+	
+	void OnCollisionEnter2D(Collision2D collision) {
+		Vector2 tweak = new Vector2 (Random.Range(0f,0.2f),Random.Range (0f,0.2f));
+		rigidbody2D.velocity += tweak;
+		
+		if (hasStarted) {
+			audio.Play();
+		}
+	}
+}
---
+++ blockBreaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("Level Load requested for: " + name );
+		Application.LoadLevel(name);
+		Brick.breakableCount = 0;
+	}
+	
+	public void QuitRequest(){
+		Debug.Log("I want to quit!");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel(){
+		Application.LoadLevel (Application.loadedLevel + 1);
+		Brick.breakableCount = 0;
+		
+	}
+	
+	public void BrickDestroyed(){
+		if (Brick.breakableCount <= 0) {
+			LoadNextLevel();
+		}
+	}
+}
